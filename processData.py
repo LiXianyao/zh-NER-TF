59bc8a46a1d53f4a2b4e1b2e9077c5b8bc160080
LiXianyao <buptlixianyao@163.com>
2019-05-24 11:37:23 +0800

    金融数据的处理完毕，校验中

--- processData.py
+++ processData.py
@@ -13,16 +13,18 @@ MSRA_data = {
 }
 
 Stock_data = {
-    "pattern": "/o|/n[srt]",
-    "tag": {"/o": "O", "/nr": "PER", "/ns": "LOC", "/nt": "ORG"},
-    "other": "/o"
+    "pattern": "</o>|</n[oifnptmlc]>",
+    "tag": {"</o>": "O", "</ni>": "PROD", "</nf>": "FIE", "</no>": "ORG",
+            "</nn>": "NUM", "</np>": "PERC", "</nt>": "TIME", "</nm>": "MERT",
+            "</nl>": "LOC", "</nc>": "ISO"},
+    "other": "</o>"
 }
 
 
 def original2inputFile(original_path, input_path, data_schema=MSRA_data, tag_schema="BIO"):
     delimiter, labels, other_tag = data_schema["pattern"], data_schema["tag"], data_schema["other"]
     with open(original_path, "r") as original_file:
-        with open(input_path, "w") as input_file:
+        with open(input_path, "w", encoding="utf8") as input_file:
             line_cnt = 0
             suc_sen_cnt = 0
             entity_cnt = dict(zip(labels.keys(), [0]*len(labels)))
@@ -38,6 +40,9 @@ def original2inputFile(original_path, input_path, data_schema=MSRA_data, tag_sch
                     logger.error("第%d行的标注数据解析异常，原因：标签数与拆解后的文本段数不和，文本内容：%s"%(line_cnt, line))
                 if len(word_list[-1]):  # 有些人标的数据少了结尾的最后一处，补上个o
                     tag_list.append(other_tag)  # 在去掉了空格的正常情况下，这里的最后一个应该是一个空字符串，不是的话说明没有标注到句尾
+                if not len(word_list[0].strip()):  # 句首多了个标签
+                    del tag_list[0]
+                    del word_list[0]
                 char_list, char_tag_list, entity_cnt = sent2char(word_list, tag_list, other_tag, entity_cnt, labels, tag_schema)
                 if char_list and char_tag_list:
                     suc_sen_cnt += 1
@@ -93,18 +98,20 @@ if __name__=="__main__":
     #"""
     MSRA_original = "data_path/original/testright1.txt"
     MSRA_input = "data_path/MSRA/test_data"
-    Stock_original = "Stock/Stock_data/train_10.txt"
+    Stock_original = "Stock/Stock_data/train_part"
     Stock_input = "Stock/Stock_data/train_data"
     #original2inputFile(original_path=MSRA_original, input_path=MSRA_input)
     # MSRA训练集：各类实体的数据情况如下：{'/nr': 17615, '/ns': 36517, '/nt': 20571, '/o': 1193462}
     # 各类实体的数据情况如下：{'/nr': 1973, '/ns': 2877, '/o': 8786, '/nt': 1331}
     # cat train_data | grep -n ^.$ > record
 
-    original2inputFile(original_path=Stock_original, input_path=Stock_input)
+    original2inputFile(original_path=Stock_original, input_path=Stock_input, data_schema=Stock_data)
     """
     vec_path = "data_path/vocb/joint4.all.b10c1.2h.iter17.mchar"
     data_path = "MSRA_data/MSRA/"
     vec2id(vec_path, data_path, name="joint4")
     ""#"
     python3 -u main.py --mode=train --train_data=MSRA_data/MSRA/ --test_data=MSRA_data/MSRA/ --update_embedding=True --pretrain_embedding=joint4.npy --unk='-unknown-' --word2id=joint4.pkl --clip=100.0 --epoch=100
+    
+    python3 -u main.py --mode=demo --train_data=MSRA_data/MSRA/  --demo_model=201905232232 --pretrain_embedding=joint4.npy --unk='-unknown-' --word2id=joint4.pkl
     """
