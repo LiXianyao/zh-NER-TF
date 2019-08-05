62d959f3956d5f8dc0f8cc18fbd6da5a74de3700
LiXianyao <buptlixianyao@163.com>
2019-08-05 21:23:22 +0800

    发现sqlalchemy只写要用到的列就可以使用了=、=

--- Stock/entity_mark.py
+++ Stock/entity_mark.py
@@ -9,9 +9,6 @@ class EntityMark(Base):
     id = Column(Integer,autoincrement=True, primary_key=True)
     content = Column(Text, nullable=False)
     passed = Column(Boolean, default=False)  # 是否审核通过
-    reviewed = Column(Boolean, default=False)  # 是否被审核过
-    ver_date = Column(Date, nullable=True)
-    user_id = Column(Integer, nullable=True)  # 是审核数据的人的id！不是数据上传人
     # 查询构造器、、、
     query = db_session.query_property()
 
